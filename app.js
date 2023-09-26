9812bb0ceeeea8aa209cb93d89addbb8833005e7
christoffersaietz <chsa22ad@student.cbs.dk>
2023-09-26 14:05:44 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-const http = require('http');
+const http = require("http")
 
-const hostname = '0.0.0.0';
-const port = 3000;
+const hostname = "0.0.0.0"
+const port = 3000
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hej det gode hold\n');
-});
+  res.statusCode = 200
+  res.setHeader("Content-Type", "text/plain")
+  res.end("Hej det dårlige hold\n")
+})
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
-
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
