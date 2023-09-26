177a6d6a25f53bb2c95de0de6f48ccfb679c7942
christoffersaietz <chsa22ad@student.cbs.dk>
2023-09-26 14:37:54 +0200

    Update server.js

--- kode/server/server.js
+++ kode/server/server.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const cors = require("cors");
+const express = require("express")
+const cors = require("cors")
 
-const app = express();
+const app = express()
 
-const customerRoute = require("./routes/customer");
+const customerRoute = require("./routes/customer")
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-app.use("/customer", customerRoute);
+app.use("/customer", customerRoute)
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+  res.send("Hello World")
+})
 
 app.listen(3000, () => {
-  console.log("Server open on port 3000");
-});
+  console.log("Server open on port 3000")
+})
